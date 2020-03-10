1b9b62269fd8a5d02c9113cc0ee4463668847923
fauh45 <fauzanfauh45@gmail.com>
2020-03-10 16:00:41 +0700

    fitment to purpose

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import jsonify
+from bs4 import BeautifulSoup
+import requests
+import time
+import re
+import json
+from datetime import datetime
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+@application.route("/api")
+def getJson():
+    with open("dump.json") as json_file:
+        return jsonify(json_file)
 
 if __name__ == "__main__":
     application.run()
